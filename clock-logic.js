5eb4b73a0628c0ec5e6565dc0bb0c03786dbb0f7
Jackson Ming Hu <huming2207@gmail.com>
2017-04-08 23:48:49 +1000

    Initial commit

---
+++ clock-logic.js
@@ -0,0 +1,9 @@
+'use-strict'
+
+var clockTimerToken = setInterval(clockTimer, 1000);
+
+function clockTimer() {
+    var currentDate = new Date();
+    document.getElementById("time-string").innerHTML = currentDate.toLocaleTimeString();
+    document.getElementById("week-string").innerHTML = currentDate.toDateString();
+}
