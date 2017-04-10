43e07335bb05ed6bb39e3012ee0be6c7bdc9dba8
Jackson Ming Hu <huming2207@gmail.com>
2017-04-10 22:36:55 +1000

    First deployment commit

--- yahoo-weather.js
+++ yahoo-weather.js
@@ -1,9 +1,8 @@
-var weather_forecast;
 function loadWeather(){
     $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22melbourne%2C%20victoria%22)%20and%20u%3D'c'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=", 
     function(data) {
         // Update weather
-        $("#weather-today").text(data.query.results.channel.item.condition.temp + " ºC");
+        $("#weather-today").text(data.query.results.channel.item.condition.temp + "ºC");
         $("#weather-tomorrow").text(data.query.results.channel.item.forecast["0"].low + "~" + data.query.results.channel.item.forecast["0"].high + "ºC");
         $("#weather-status-today").text(data.query.results.channel.item.condition.text);
         $("#weather-status-tomorrow").text(data.query.results.channel.item.forecast["0"].text);
